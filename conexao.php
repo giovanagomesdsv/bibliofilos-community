4f7a72564fb3b345472ab97552a236f110dc1690
MarianaArissa123 <arissamariana66@gmail.com>
2025-06-02 08:18:26 -0300

    conexao

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php 
 $hostname = "localhost";
 $banco = "bd_tcc_etim_123_g2";
-$usuario = "us_tcc_etim_123_g2";
+$usuario = "us_etim_123_g2";
 $senha = "ec0623";
 
 $conn = new mysqli($hostname, $usuario, $senha, $banco);
