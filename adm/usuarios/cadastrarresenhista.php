f2fafa283a0b5c33894ecd407346c3c6226a462f
Giovana Gomes <g.gomesdsv@gmail.com>
2025-06-03 12:33:20 -0300

    tyjt

--- adm/usuarios/cadastrarresenhista.php
+++ adm/usuarios/cadastrarresenhista.php
@@ -93,7 +93,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Resenhistas</title>
     <link rel="stylesheet" href="../geral.css">
-    <link rel="stylesheet" href="usuarios.css">
+    <link rel="stylesheet" href="usuariose.css">
 </head>
 
 <body>
