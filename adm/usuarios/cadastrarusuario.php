50757adf3530d61825604482f1c9bc571222d992
MarianaArissa123 <arissamariana66@gmail.com>
2025-10-20 15:02:30 -0300

    finalizado

--- adm/usuarios/cadastrarusuario.php
+++ adm/usuarios/cadastrarusuario.php
@@ -45,12 +45,75 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastrar Usuário - BACKSTAGE Community</title>
-    <link rel="stylesheet" href="usuarios.css">
+
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href='https://cdn.boxicons.com/fonts/basic/boxicons.min.css' rel='stylesheet'>
+    <link href='https://cdn.boxicons.com/fonts/brands/boxicons-brands.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="../geral.css">
+    <link rel="stylesheet" href="usuarios.css">
+    <title>Cadastrar Usuarios - BACKSTAGE Community</title>
 </head>
 
+
 <body>
+        <header>
+        BACKSTAGE Community
+    </header>
+    <nav class="sidebar" id="sidebar">
+    <ul class="nav-list">
+        <div class="nome">
+            <li>
+                <a href="../perfil/perfil.php">
+                    <span class="link_name">
+                        <?php echo $_SESSION['nome']; ?>
+                    </span>
+                </a>
+            </li>
+            <div class="menu" id="menu">
+                <i class="bx bx-menu"></i>
+            </div>
+        </div>
+        <li>
+                <a href="../../index.php">
+                <i class='bx  bx-reply-stroke'></i> 
+                    <span class="link_name">Bibliófilos Community</span>
+                </a>
+            </li>
+            <li>
+                <a href="../home.php">
+                    <i class='bx bx-home-alt-2'></i>
+                    <span class="link_name">Home</span>
+                </a>
+            </li>
+            <li>
+                <a href="../livrarias/livrarias.php">
+                    <i class='bx bx-user'></i>
+                    <span class="link_name">Livrarias</span>
+                </a>
+            </li>
+            <li>
+                <a href="../resenhistas/resenhistas.php">
+                    <i class='bx bx-user-pin'></i>
+                    <span class="link_name">Resenhistas</span>
+                </a>
+            </li>
+            <li>
+                <a href="../livro/livros.php">
+                    <i class='bx bx-book-bookmark'></i>
+                    <span class="link_name">Livros</span>
+                </a>
+            </li>
+            <li class="fix">
+                <a href="usuarios.php">
+                    <i class='bx bx-book-content'></i>
+                    <span class="link_name">Usuarios</span>
+                </a>
+            </li>
+            <li class="sair">
+                <a href="../logout.php"><i class='bx bx-log-out'></i></a>
+            </li>
+        </ul>
+    </nav>
     <form action="cadastrarusuario.php" method="POST" class='format2'>
         <label for="email">E-mail:</label>
         <input type="email" name="email" required class='inputEditar2'>
@@ -70,6 +133,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
         <input type="submit" value="Cadastrar usuário" class='inputConfirmar'>
     </form>
+        <script src="../script.js"></script>
+
 </body>
 
 </html>
