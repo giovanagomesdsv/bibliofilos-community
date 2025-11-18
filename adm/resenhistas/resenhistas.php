57730b367806002ecf7d03e73e5cdaa3195010c1
MarianaArissa123 <arissamariana66@gmail.com>
2025-11-18 09:55:45 -0300

    t

--- adm/resenhistas/resenhistas.php
+++ adm/resenhistas/resenhistas.php
@@ -13,6 +13,8 @@ include "../protecao.php";
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link href='https://cdn.boxicons.com/fonts/basic/boxicons.min.css' rel='stylesheet'>
     <link href='https://cdn.boxicons.com/fonts/brands/boxicons-brands.min.css' rel='stylesheet'>
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
+
     <link rel="stylesheet" href="../geral.css">
     <link rel="stylesheet" href="resenhistas.css">
      <title>Resenhistas - BACKSTAGE Community</title>
