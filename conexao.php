3c94b40b30a5bec535d2ebf0dfea9313fdd4ec13
giovanagomesdsv <g.gomesdsv@gmail.com>
2026-02-05 18:23:17 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -4,7 +4,7 @@ $banco = "bd_tcc_etim_123_g2";
 $usuario = "root";
 $senha = "";
 
-$conn = new mysqli($hostname, $usuario, $senha, $banco);
+$conn = new mysqli($hostname, $usuario, $senha, $banco); 
 
 if ($conn->connect_errno) {
     echo "Falha ao se conectar: ". $conn->connect_errno." ->". $conn->connect_error;
